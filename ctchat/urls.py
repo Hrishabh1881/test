a8139cf26e7101bbb18fe4f375e4f298d0af6bb8
SuryaBhosale <surya@theqindia.com>
2024-06-17 14:53:58 +0530

    removing unwanted lines of code

--- ctchat/urls.py
+++ ctchat/urls.py
@@ -4,9 +4,5 @@ from django.urls import path, include
 
 
 urlpatterns = [
-    path('clinicaltrialsinfo', ClinicalTrialsLLMView.as_view(), name='clinical-trial-info'),
-    path('clinicaltrialsinfo2', ClinicalTrialsLLMViewHybrid.as_view(), name='clinical-trial-info-hybrid'),
-    path('clinicaltrialsinfo3', ClinicalTrialsLLMViewHybridLocationList.as_view(), name='clinical-trial-location-list'),
     path('clinicaltrialwithzip', ClinicalTrialsLLMViewHybridZipLocator.as_view(), name='clinical-trial-zip'),
-    path('filtertrials', FilterClinicalTrialsDatabseView.as_view(), name='directfilter'),
     path('clinicaltrialsdetails', GetClinicalTrialDetailsView.as_view(), name='detailsview')]
